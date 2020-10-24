65a3243266a301aec18733010aa75f949a622cb8
Leo Liang <leoliang@gmail.com>
2020-10-24 13:19:19 +0800

    Improve Garmin sync
    - Change to asyncio, remove multithread, limit concurrent downloads
    - Add loading configuration from file
    - Remove unused code

--- scripts/config.py
+++ scripts/config.py
@@ -1,11 +1,30 @@
 import os
+import yaml
 
 GET_DIR = "activities"
-BASE_URL = "https://api.nike.com/sport/v3/me"
-TOKEN_REFRESH_URL = "https://unite.nike.com/tokenRefresh"
 OUTPUT_DIR = "activities"
 GPX_FOLDER = os.path.join(os.getcwd(), "GPX_OUT")
 GPX_TEST = os.path.join(os.getcwd(), "GPX_TEST")
-NIKE_CLIENT_ID = "HlHa2Cje3ctlaOqnxvgZXNaAs7T9nAuH"
 SQL_FILE = "scripts/data.db"
 JSON_FILE = "src/static/activities.js"
+
+# TODO: Move into nike_sync
+BASE_URL = "https://api.nike.com/sport/v3/me"
+TOKEN_REFRESH_URL = "https://unite.nike.com/tokenRefresh"
+NIKE_CLIENT_ID = "HlHa2Cje3ctlaOqnxvgZXNaAs7T9nAuH"
+
+try:
+    with open('config.yaml') as f:
+        _config = yaml.safe_load(f)
+except:
+    _config = {}
+
+def config(*keys):
+    def safeget(dct, *keys):
+        for key in keys:
+            try:
+                dct = dct[key]
+            except KeyError:
+                return None
+        return dct
+    return safeget(_config, *keys)
