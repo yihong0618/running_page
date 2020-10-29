4ca2afe6d2a497161af8001f3c1518b4280fee21
SunShinenny <sunshinenny@icloud.com>
2020-10-29 22:53:30 +0800

    update token

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,5 @@
 // const
-const MAPBOX_TOKEN = 'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoic3Vuc2hpbmVubnkiLCJhIjoiY2tndXhuc2hvMW52bzJzcjNrNjBtcDkybSJ9.oQYemUsp_3o0Vorp_JCZmg';
 const MUNICIPALITY_CITIES_ARR = ['北京市', '上海市', '天津市', '重庆市', '香港特别行政区', '澳门特别行政区'];
 const IS_CHINESE = true
 
