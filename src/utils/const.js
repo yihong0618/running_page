ba8022b4eb55f52d8dff31ff4d1f4261f6ec5331
zhaohongxuan <hxzhenu@gmail.com>
2020-10-30 18:07:45 +0800

    init

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,5 @@
 // const
-const MAPBOX_TOKEN = 'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoiemhhb2hvbmd4dWFuIiwiYSI6ImNrZ3cyczRoNDA2NzkyeXA2NXlycjVza28ifQ.zT5wZLC44B9i9c3rbt-trA';
 const MUNICIPALITY_CITIES_ARR = ['北京市', '上海市', '天津市', '重庆市', '香港特别行政区', '澳门特别行政区'];
 const IS_CHINESE = true
 
