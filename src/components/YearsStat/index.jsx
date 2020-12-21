82d7c57ed3de5c2e66a7e593ea644d0bc06121c8
geekplux <geekplux@gmail.com>
2020-12-21 21:52:49 +0800

    refactor: Year Stat

---
+++ src/components/YearsStat/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { INFO_MESSAGE } from 'src/utils/const';
+import YearStat from 'src/components/YearStat';
+
+const YearsStat = ({ runs, year, onClick, yearsArr }) => {
+  // make sure the year click on front
+  let yearsArrayUpdate = yearsArr.slice();
+  yearsArrayUpdate = yearsArrayUpdate.filter((x) => x !== year);
+  yearsArrayUpdate.unshift(year);
+
+  // for short solution need to refactor
+  return (
+    <div className="fl w-100 w-30-l pb5 pr5-l">
+      <section className="pb4" style={{ paddingBottom: '0rem' }}>
+        <p>
+          {INFO_MESSAGE(yearsArr.length, year)}
+          <br />
+        </p>
+      </section>
+      <hr color="red" />
+      {yearsArrayUpdate.map((year) => (
+        <YearStat
+          yearsArr={yearsArr}
+          key={year}
+          runs={runs}
+          year={year}
+          onClick={onClick}
+        />
+      ))}
+      <YearStat
+        yearsArr={yearsArr}
+        key="Total"
+        runs={runs}
+        year="Total"
+        onClick={onClick}
+      />
+    </div>
+  );
+};
+
+export default YearsStat;
