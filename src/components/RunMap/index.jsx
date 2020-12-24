a6245e580cca3c0931a1428ceb1f95e03f28aa52
geekplux <geekplux@gmail.com>
2020-12-25 01:50:00 +0800

    refactor: RunMap

---
+++ src/components/RunMap/index.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import MapboxLanguage from '@mapbox/mapbox-gl-language';
+import ReactMapGL, { Source, Layer } from 'react-map-gl';
+import {
+  MAPBOX_TOKEN,
+  IS_CHINESE,
+  MAIN_COLOR,
+  PROVINCE_FILL_COLOR,
+} from 'src/utils/const';
+import RunMapButtons from './RunMapButtons';
+import RunMarker from './RunMaker';
+import styles from './style.module.scss';
+
+const RunMap = ({
+  title,
+  viewport,
+  setViewport,
+  changeYear,
+  geoData,
+  yearsArr,
+  thisYear,
+  provinces,
+}) => {
+  const addControlHandler = (event) => {
+    const map = event && event.target;
+    // set lauguage to Chinese if you use English please comment it
+    if (map && IS_CHINESE) {
+      map.addControl(
+        new MapboxLanguage({
+          defaultLanguage: 'zh',
+        })
+      );
+      map.setLayoutProperty('country-label-lg', 'text-field', [
+        'get',
+        'name_zh',
+      ]);
+    }
+  };
+  const filterProvinces = provinces.slice();
+  // for geojson format
+  filterProvinces.unshift('in', 'name');
+
+  const isBigMap = viewport.zoom <= 3;
+  if (isBigMap && IS_CHINESE) {
+    geoData = geoJsonForMap();
+  }
+
+  const isSingleRun =
+    geoData.features.length === 1 &&
+    geoData.features[0].geometry.coordinates.length;
+  let startLon;
+  let startLat;
+  let endLon;
+  let endLat;
+  if (isSingleRun) {
+    const points = geoData.features[0].geometry.coordinates;
+    [startLon, startLat] = points[0];
+    [endLon, endLat] = points[points.length - 1];
+  }
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      mapStyle="mapbox://styles/mapbox/dark-v9"
+      onViewportChange={setViewport}
+      onLoad={addControlHandler}
+      mapboxApiAccessToken={MAPBOX_TOKEN}
+    >
+      <RunMapButtons
+        changeYear={changeYear}
+        yearsArr={yearsArr}
+        thisYear={thisYear}
+      />
+      <Source id="data" type="geojson" data={geoData}>
+        <Layer
+          id="prvince"
+          type="fill"
+          paint={{
+            'fill-color': PROVINCE_FILL_COLOR,
+          }}
+          filter={filterProvinces}
+        />
+        <Layer
+          id="runs2"
+          type="line"
+          paint={{
+            'line-color': MAIN_COLOR,
+            'line-width': isBigMap ? 1 : 2,
+          }}
+          layout={{
+            'line-join': 'round',
+            'line-cap': 'round',
+          }}
+        />
+      </Source>
+      {isSingleRun && (
+        <RunMarker
+          startLat={startLat}
+          startLon={startLon}
+          endLat={endLat}
+          endLon={endLon}
+        />
+      )}
+      <span className={styles.runTitle}>{title}</span>
+    </ReactMapGL>
+  );
+};
+
+export default RunMap;
