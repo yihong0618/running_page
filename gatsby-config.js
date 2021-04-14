5e01bc34a5b318b146101562ecd381638455bc22
KANGNAMEMEDA <47210006+KANGNAMEMEDA@users.noreply.github.com>
2021-04-14 11:24:30 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/`, // Change to `/running_page` when running on github pages
+  pathPrefix: `/running_page`, // Change to `/running_page` when running on github pages
   siteMetadata: {
     title: 'Running page',
     siteUrl: 'https://yihong.run',
