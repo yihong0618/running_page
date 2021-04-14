703fca446f75a2d117c46415bc1ce9ab9d6c0e21
KANGNAMEMEDA <47210006+KANGNAMEMEDA@users.noreply.github.com>
2021-04-14 17:28:20 +0800

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -54,10 +54,10 @@ export {
 
 export const AVATAR =
   'https://raw.githubusercontent.com/KANGNAMEMEDA/-/main/1.JPG'; // Temp avatar
-export const NAVS = [
-  //{ text: 'Blog', link: 'https://github.com/yihong0618/running_page' },
-  { text: 'RUNNING', link: 'https://kangnamemeda.github.io/running_page/' },
-];
+//export const NAVS = [
+//  { text: 'Blog', link: 'https://github.com/yihong0618/running_page' },
+//  { text: 'RUNNING', link: 'https://kangnamemeda.github.io/running_page/' },
+//];
 
 const nike = 'rgb(224,237,94)'; // if you want change the main color change here src/styles/variables.scss
 export const MAIN_COLOR = nike;
