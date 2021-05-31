742ba2634a36689dee55498febfa757d652bab9d
yihong0618 <zouzou0208@gmail.com>
2021-05-31 09:43:26 +0800

    fix: now only run in strava

--- scripts/generator/__init__.py
+++ scripts/generator/__init__.py
@@ -53,13 +53,13 @@ class Generator:
                 filters = {"before": datetime.datetime.utcnow()}
 
         for run_activity in self.client.get_activities(**filters):
-            created = update_or_create_activity(self.session, run_activity)
-            if created:
-                sys.stdout.write("+")
-            else:
-                sys.stdout.write(".")
-            sys.stdout.flush()
-
+            if run_activity.type == "Run":
+                created = update_or_create_activity(self.session, run_activity)
+                if created:
+                    sys.stdout.write("+")
+                else:
+                    sys.stdout.write(".")
+                sys.stdout.flush()
         self.session.commit()
 
     def sync_from_gpx(self, gpx_dir):
