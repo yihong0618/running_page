d1dd43f9b984b5bd07aa20554c2bf0ca3d39729b
Fanyang Meng <i@mfy.dev>
2021-06-07 21:40:32 +0800

    Fix favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
         background_color: '#e1e1e1',
         theme_color: '#e1e1e1',
         display: 'minimal-ui',
-        icon: 'src/images/favicon.ico', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-transformer-sharp',
