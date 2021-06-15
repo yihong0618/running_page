8aacb70ac4374553857446a7101a915d9111f2a2
zhaohongxuan <hxzhenu@gmail.com>
2021-06-15 00:19:52 +0000

    Add new runs

--- src/static/activities.js
+++ src/static/activities.js
@@ -15160,6 +15160,20 @@ const activities = [
     "average_heartrate": 155.0,
     "average_speed": 3.3818849978634065,
     "streak": 2
+  },
+  {
+    "run_id": 9223370413182843607,
+    "name": "run from keep",
+    "distance": 5326.6953125,
+    "moving_time": "0:25:01",
+    "type": "Run",
+    "start_date": "2021-06-14 11:33:46",
+    "start_date_local": "2021-06-14 19:33:46",
+    "location_country": "{'latitude': 31.24174690246582, 'longitude': 121.67061614990234, 'country': '\u4e2d\u56fd', 'nationCode': '156', 'province': '\u4e0a\u6d77\u5e02', 'city': '\u4e0a\u6d77\u5e02', 'cityCode': '021', 'district': '\u6d66\u4e1c\u65b0\u533a', 'districtCode': '310115'}",
+    "summary_polyline": "",
+    "average_heartrate": 166.0,
+    "average_speed": 3.548764365423051,
+    "streak": 1
   }
 ];
 
