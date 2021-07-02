38e424f13dba251601d4e1a5d27e899d0fee1f53
ustccuikai <ustccuikai@126.com>
2021-07-02 16:28:01 +0800

    sync original format file from strava to garmin

--- scripts/strava_to_garmin_sync.py
+++ scripts/strava_to_garmin_sync.py
@@ -1,13 +1,15 @@
 import argparse
 import asyncio
+import pytz
 from io import BytesIO
 import gpxpy
 from xml.etree import ElementTree
 import gpxpy.gpx
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from utils import make_strava_client
 from garmin_sync import Garmin
 from strava_sync import run_strava_sync
+from stravaweblib import WebClient, DataFormat
 
 from config import STRAVA_GARMIN_TYPE_DICT
 
@@ -72,7 +74,7 @@ def make_gpx_from_points(title, points_dict_list):
     return gpx.to_xml()
 
 
-async def upload_to_activities(garmin_client, strava_client):
+async def upload_to_activities(garmin_client, strava_client, strava_web_client):
     last_activity = await garmin_client.get_activities(0, 1)
     if not last_activity:
         filters = {}
@@ -80,23 +82,21 @@ async def upload_to_activities(garmin_client, strava_client):
         # is this startTimeGMT must have ?
         after_datetime_str = last_activity[0]["startTimeGMT"]
         after_datetime = datetime.strptime(after_datetime_str, "%Y-%m-%d %H:%M:%S")
+        after_datetime = (
+            after_datetime.replace(tzinfo=timezone.utc)
+            .astimezone(tz=pytz.timezone("Asia/Shanghai"))
+            .strftime("%Y-%m-%d %H:%M:%S")
+        )
+        print(after_datetime)
         filters = {"after": after_datetime}
     strava_activities = list(strava_client.get_activities(**filters))
     files_list = []
     # strava rate limit
     for i in strava_activities[:50]:
-        start_time = i.start_date
-        activity_type = i.type
-        # strava steams types info from strava api doc
-        types = ["time", "latlng", "altitude", "heartrate", "velocity_smooth", "temp"]
-        s = strava_client.get_activity_streams(i.id, types=types, resolution="medium")
-        points = generate_strava_run_points(start_time, s)
-        if points:
-            garmin_type = STRAVA_GARMIN_TYPE_DICT.get(activity_type, "running")
-            gpx_doc = make_gpx_from_points("test", points)
-            file = BytesIO(bytes(gpx_doc, "utf8"))
-            files_list.append((file, garmin_type))
-    await garmin_client.upload_activities(files_list)
+        print(i.id)
+        data = strava_web_client.get_activity_data(i.id, fmt=DataFormat.ORIGINAL)
+        files_list.append(data)
+    await garmin_client.upload_activities_fit(files_list)
     return files_list
 
 
@@ -107,6 +107,8 @@ if __name__ == "__main__":
     parser.add_argument("strava_refresh_token", help="strava refresh token")
     parser.add_argument("garmin_email", nargs="?", help="email of garmin")
     parser.add_argument("garmin_password", nargs="?", help="password of garmin")
+    parser.add_argument("strava_email", nargs="?", help="email of strava")
+    parser.add_argument("strava_password", nargs="?", help="password of strava")
     parser.add_argument(
         "--is-cn",
         dest="is_cn",
@@ -119,13 +121,21 @@ if __name__ == "__main__":
         options.strava_client_secret,
         options.strava_refresh_token,
     )
+    strava_web_client = WebClient(
+        access_token=strava_client.access_token,
+        email=options.strava_email,
+        password=options.strava_password,
+    )
+
     garmin_auth_domain = "CN" if options.is_cn else ""
 
     garmin_client = Garmin(
         options.garmin_email, options.garmin_password, garmin_auth_domain
     )
     loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(upload_to_activities(garmin_client, strava_client))
+    future = asyncio.ensure_future(
+        upload_to_activities(garmin_client, strava_client, strava_web_client)
+    )
     loop.run_until_complete(future)
 
     # Run the strava sync
