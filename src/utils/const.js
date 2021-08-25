967c329792474f742aa40f72eacdf8d310526e90
superleeyom <635709492@qq.com>
2021-08-25 10:49:23 +0800

    refactor: Convert 'Total' to Chinese

--- src/utils/const.js
+++ src/utils/const.js
@@ -14,7 +14,7 @@ const MUNICIPALITY_CITIES_ARR = [
 const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength, year) =>
-  `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是 ${year === 'Total' ? '全部' : year} 的数据`;
+  `我用 App 记录自己跑步 ${yearLength} 年了，下面列表展示的是 ${year === 'Total' ? '所有' : year} 的数据`;
 const ENGLISH_INFO_MESSAGE = (yearLength, year) =>
   `Running Journey with ${yearLength} Years, the table shows year ${year} data`;
 
