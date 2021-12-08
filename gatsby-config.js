d6db3a48f643fe90e43e414107294afe68c97429
lvjikun <lvjikun@jd.com>
2021-12-08 18:19:14 +0800

    【新增】logo链接

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: `/`, // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Thomas\' Running Page',
-    siteUrl: 'https://thomaslv.run',
+    siteUrl: 'https://github.com/Thomas-Lv',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
