52f81151f38a4faf711079f9245ef436e4165f5c
yihong0618 <zouzou0208@gmail.com>
2022-01-19 15:15:13 +0800

    fix: # Chinese map offset

--- src/utils/const.js
+++ src/utils/const.js
@@ -56,5 +56,9 @@ export {
 };
 
 const nike = 'rgb(224,237,94)'; // if you want change the main color change here src/styles/variables.scss
+
+// If your map has an offset please change this line
+// issues #92 and #198
+export const NEED_FIX_MAP = false;
 export const MAIN_COLOR = nike;
 export const PROVINCE_FILL_COLOR = '#47b8e0';
