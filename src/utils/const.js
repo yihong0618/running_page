8a5bdd51cb321452bb8b8bc3c463b7f48d706536
Chase Wang <43170703+wwwwwfp@users.noreply.github.com>
2022-04-03 22:28:55 +0800

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,5 @@
 // const
-const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+const MAPBOX_TOKEN = 'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
