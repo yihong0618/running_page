26fe3c7a6cf3face956b493e34eabc5e92daed23
lvjikun <lvjikun@jd.com>
2022-05-18 10:38:31 +0800

    【修改】githubpage 部署网站

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/`, // Change to `/running_page` when running on github pages
+  pathPrefix: `/running_page`, // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Thomas\' Running Page',
     siteUrl: 'https://running-page-thomas-lv.vercel.app/',
