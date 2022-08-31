65da88e6aa77cde8d8e7125483ccb45a97ebc14a
w298102526 <298102526@qq.com>
2022-08-31 21:52:59 +0800

    gatsby-config_update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: 'running_page', // Change to `/running_page` when running on github pages
+  pathPrefix: `/running_page`, // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Running Page',
     siteUrl: 'https://run.madless.club',
@@ -8,7 +8,7 @@ module.exports = {
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://run.madless.clu',
+        url: 'https://run.madless.club',
       },
       {
         name: 'About',
