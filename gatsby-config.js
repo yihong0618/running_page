36500c5743d21a83720bceeaa34dc4e4cf20d525
lizhimiao <untildied@gmail.com>
2023-01-30 21:09:57 +0800

    update pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
+  pathPrefix: '/', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'lizhimiao\'s Running Page',
     siteUrl: 'https://lizhimiao.com/',
@@ -59,7 +59,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /assets/,
+          include: /assets/
         },
       },
     },
