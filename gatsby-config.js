450dedad71118f51b9be3cafd4ebb112e94fffe3
YeasyHeart <nyy0919@163.com>
2023-02-27 13:35:13 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,17 @@ module.exports = {
   pathPrefix: '/running_page',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://yeasyheart.github.io/running_page',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://github.com/YeasyHeart',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://github.com/YeasyHeart/running_page/blob/master/README-CN.md',
       },
     ],
   },
