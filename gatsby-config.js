8a75779cd8737017ed8ab17f1a649921a4510475
liuweiqiang <liuweiqiang316@163.com>
2023-05-05 02:20:17 +0800

    feat: init

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,12 @@ module.exports = {
   siteMetadata: {
     siteTitle: 'Running Page',
     siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    logo: 'https://avatars.githubusercontent.com/u/34744016',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
-      },
-      {
-        name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://liuweiqiang316.cn/',
       },
     ],
   },
--- scripts/strava_sync.py
+++ scripts/strava_sync.py
@@ -9,7 +9,7 @@ def run_strava_sync(client_id, client_secret, refresh_token):
     generator = Generator(SQL_FILE)
     generator.set_strava_config(client_id, client_secret, refresh_token)
     # if you want to refresh data change False to True
-    generator.sync(False)
+    generator.sync(True)
 
     activities_list = generator.load()
     with open(JSON_FILE, "w") as f:
