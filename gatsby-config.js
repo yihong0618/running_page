52aec7327b3bc6fc8bb1ecd34afb8f1dfae0d973
Gao Hao (高浩) <gaohao@home.swjtu.edu.cn>
2023-06-07 13:06:43 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,17 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'GAOHAO \'s Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    siteUrl: 'https://efish2002.github.io/running_page/',
+    logo: 'https://avatars.githubusercontent.com/u/10509616?s=400&u=e356a1490ddac5624847d822af8f0faec4892ab4&v=4',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://efish2002.github.io/running_page/',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://github.com/efish2002/running_page/blob/master/README-CN.md',
       },
     ],
   },
