aacd3a1cfdd85ad4ca307a9b4ecac0f0f71faebe
Gao Hao (高浩) <gaohao@home.swjtu.edu.cn>
2023-06-15 14:13:53 +0800

    Update const.js
    test

--- src/utils/const.js
+++ src/utils/const.js
@@ -27,13 +27,13 @@ const MAP_LAYER_LIST = [
 ];
 
 // styling: set to `true` if you want dash-line route
-const USE_DASH_LINE = true;
+const USE_DASH_LINE = false;
 // styling: route line opacity: [0, 1]
 const LINE_OPACITY = 0.4;
 // styling: map height
 const MAP_HEIGHT = 600;
 //set to `false` if you want to hide the road label characters
-const ROAD_LABEL_DISPLAY = true;
+const ROAD_LABEL_DISPLAY = false;
 
 // IF you outside China please make sure IS_CHINESE = false
 const IS_CHINESE = true;
