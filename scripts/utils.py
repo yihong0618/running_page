1128a62aa53dd8c13760fe45005817dfd7d8f167
Zhuohui Yu <agassi21@gmail.com>
2023-07-18 18:23:53 +0800

    fix: add try except when load json

--- scripts/utils.py
+++ scripts/utils.py
@@ -112,9 +112,14 @@ def save_synced_data_file_list(data_dir: str, file_list: list):
     data_dir = os.path.abspath(data_dir)
     synced_json_file = os.path.join(data_dir, SYNCED_FILE_NAME)
 
+    old_list = []
     if os.path.exists(synced_json_file):
         with open(synced_json_file, "r") as f:
-            old_list = json.load(f)
+            try:
+                old_list = json.load(f)
+            except Exception as e:
+                print(f"json load {synced_json_file} \nerror {e}")
+                pass
 
     with open(synced_json_file, "w") as f:
         file_list.extend(old_list)
