d0ea0ff9858ffba0bc0f878b55d2b49907252382
Zhuohui Yu <agassi21@gmail.com>
2023-07-18 18:08:21 +0800

    reformated

--- scripts/utils.py
+++ scripts/utils.py
@@ -107,18 +107,20 @@ def upload_file_to_strava(client, file_name, data_type):
             f"Uploading {data_type} file: {file_name} to strava, upload_id: {r.upload_id}."
         )
 
+
 def save_synced_data_file_list(data_dir: str, file_list: list):
     data_dir = os.path.abspath(data_dir)
     synced_json_file = os.path.join(data_dir, SYNCED_FILE_NAME)
 
     if os.path.exists(synced_json_file):
-        with open(synced_json_file, 'r') as f:
-          old_list = json.load(f)
+        with open(synced_json_file, "r") as f:
+            old_list = json.load(f)
 
     with open(synced_json_file, "w") as f:
         file_list.extend(old_list)
 
         json.dump(file_list, f)
+
 
 def load_synced_file_list(data_dir: str):
     data_dir = os.path.abspath(data_dir)
@@ -127,7 +129,7 @@ def load_synced_file_list(data_dir: str):
     if os.path.exists(synced_json_file):
         with open(synced_json_file, "r") as f:
             try:
-              return json.load(f)
+                return json.load(f)
             except Exception as e:
                 print(f"json load {synced_json_file} \nerror {e}")
                 pass
