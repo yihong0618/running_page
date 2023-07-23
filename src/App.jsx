91c4e2e2a621894ca9b5473f7addab5a24e9cf7f
Friends A <github@duanfei.org>
2023-07-23 10:01:15 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -7,7 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
