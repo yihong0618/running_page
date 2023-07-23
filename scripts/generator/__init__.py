bcd867ad56660ca041a019ee6ff53f500a54258f
ben_29 <ben_29@live.com>
2023-07-23 11:13:28 +0800

    fix: #454, no `file_names` when sync from app

--- scripts/generator/__init__.py
+++ scripts/generator/__init__.py
@@ -108,7 +108,8 @@ class Generator:
                 sys.stdout.write("+")
             else:
                 sys.stdout.write(".")
-            synced_files.extend(t.file_names)
+            if t.__contains__("file_names"):
+                synced_files.extend(t.file_names)
             sys.stdout.flush()
 
         self.session.commit()
