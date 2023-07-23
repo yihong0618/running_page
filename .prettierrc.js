4479e1015fbb5add0a1365c4446a64f3aa9a5c69
Friends A <github@duanfei.org>
2023-07-23 09:20:39 +0800

    init

--- .prettierrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  arrowParens: 'always',
-  singleQuote: true,
-  jsxSingleQuote: true,
-  tabWidth: 2,
-  semi: true,
-};
--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from 'next/app';
-
-import '@/styles/globals.css';
-
-import siteMetadata from '../../siteMetadata';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} siteMetadata={siteMetadata} />;
-}
