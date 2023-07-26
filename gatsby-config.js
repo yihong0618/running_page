6fdb9854c1de3e92710e031d9d966e941c458a23
Gold-Sea <1800013512@pku.edu.cn>
2023-07-27 00:51:44 +0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,13 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
+    siteUrl: 'https://zilizhang.site/running_page',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
-        name: 'Blog',
-        url: 'https://yihong.run/running',
-      },
-      {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://zilizhang.site/',
       },
     ],
   },
