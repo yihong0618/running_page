bd0c5821b03c81a44ae2bc08168bcf454434246a
Licardo <1014660822@qq.com>
2023-09-23 22:01:23 +0800

    modify settings

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,17 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  siteUrl: 'https://run.licardo.cn/',
+  logo: 'https://ghproxy.com/https://raw.githubusercontent.com/L1cardo/l1cardo.github.io/blog/themes/butterfly/source/img/avatar.png',
+  description: '我的跑步页',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: '博客',
+      url: 'https://blog.licardo.cn',
     },
     {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: '关于',
+      url: 'https://blog.licardo.cn/about',
     },
   ],
 };
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoibGljYXJkbyIsImEiOiJja2tkYWJ1bDQwOGhlMnVvNWtqMzBhcG55In0.5AtikAuJuWC4lP6dJvS6jw';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
