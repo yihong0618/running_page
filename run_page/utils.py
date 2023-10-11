657d135b5c9660d510e5addf155ce758e4d48e14
yihong0618 <zouzou0208@gmail.com>
2023-10-11 22:39:06 +0800

    fix: upload to strava type

--- run_page/utils.py
+++ run_page/utils.py
@@ -90,17 +90,28 @@ def get_strava_last_time(client, is_milliseconds=True):
         return 0
 
 
-def upload_file_to_strava(client, file_name, data_type):
+def upload_file_to_strava(client, file_name, data_type, force_to_run=True):
     with open(file_name, "rb") as f:
         try:
-            r = client.upload_activity(activity_file=f, data_type=data_type)
+            if force_to_run:
+                r = client.upload_activity(
+                    activity_file=f, data_type=data_type, activity_type="run"
+                )
+            else:
+                r = client.upload_activity(activity_file=f, data_type=data_type)
+
         except RateLimitExceeded as e:
             timeout = e.timeout
             print()
             print(f"Strava API Rate Limit Exceeded. Retry after {timeout} seconds")
             print()
             time.sleep(timeout)
-            r = client.upload_activity(activity_file=f, data_type=data_type)
+            if force_to_run:
+                r = client.upload_activity(
+                    activity_file=f, data_type=data_type, activity_type="run"
+                )
+            else:
+                r = client.upload_activity(activity_file=f, data_type=data_type)
         print(
             f"Uploading {data_type} file: {file_name} to strava, upload_id: {r.upload_id}."
         )
