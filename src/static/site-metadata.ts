c9380c53a0aac4b9b9bc5d058a43f3e5dd5710b5
NoZTurn <infoseek@163.com>
2023-11-02 14:54:33 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,16 +11,16 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://run.jiangkai.org',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'Running Page',
+      url: 'https://run.jiangkai.org',
     },
     {
-      name: 'About',
+      name: 'Thanks',
       url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
     },
   ],
