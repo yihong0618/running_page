b0f1d45e887313be59eb74a592118257a7c47dee
xulanzhong <xulanzhong@taoche.com>
2023-11-22 13:50:18 +0800

    update
    update

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,8 +12,8 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
   siteUrl: 'https://run.xmylog.com',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  logo: 'https://s1.ax1x.com/2023/09/03/pPDnMZR.jpg',
+  description: '离开乏味的皮囊,自由的灵魂在路上',
   navLinks: [
     {
       name: 'Blog',
