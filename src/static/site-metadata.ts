ce22bcfc60f90521f80cc7b33dcd2aef4d14832e
JiuGoe <lxp_xcn@hotmail.com>
2023-12-01 15:59:23 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,7 +11,7 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://running.danilelxp.com',
+  siteUrl: 'http://running.danilelxp.com',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Just running',
   navLinks: [
