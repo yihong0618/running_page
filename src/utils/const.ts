f74d0f1f0217d19bc23527f462bb50fa56900818
JiuGoe <lxp_xcn@hotmail.com>
2023-12-01 15:06:14 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1Ijoiaml1Z2UiLCJhIjoiY2xhcWs5M2lqMDFqbDNybzNtMTlzc2xmNCJ9.ImopQGZUFmVgPNQcpqcJSA';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
