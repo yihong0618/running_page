7eada8acdd7056aa24b6f666f48a82cb8e497e5f
Tianhui Xu <45302287+TianhuiXu@users.noreply.github.com>
2023-12-06 01:35:13 -0800

    Initial commit on configuration & metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  siteTitle: "Tianhui's Outdoor Journey",
+  siteUrl: 'https://outdoor-journey-tianhui.vercel.app/',
+  logo: 'assets/logo.png',
+  description: "Tianhui's Outdoor Journey",
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'Strava',
+      url: 'https://www.strava.com/athletes/105920909',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/TianhuiXu',
     },
   ],
 };
