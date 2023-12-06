aa3a57fd9eb2f098c3aece4d580696b4ff5dce22
JiuGoe <lxp_xcn@hotmail.com>
2023-12-06 15:38:45 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -53,8 +53,8 @@ const FULL_MARATHON_RUN_TITLE = IS_CHINESE ? '全程马拉松' : 'Full Marathon'
 const HALF_MARATHON_RUN_TITLE = IS_CHINESE ? '半程马拉松' : 'Half Marathon';
 const MORNING_RUN_TITLE = IS_CHINESE ? '清晨跑步' : 'Morning Run';
 const MIDDAY_RUN_TITLE = IS_CHINESE ? '午间跑步' : 'Midday Run';
-const AFTERNOON_RUN_TITLE = IS_CHINESE ? '傍晚跑步' : 'Afternoon Run';
-const EVENING_RUN_TITLE = IS_CHINESE ? '夜间跑步' : 'Evening Run';
+const AFTERNOON_RUN_TITLE = IS_CHINESE ? '午后跑步' : 'Afternoon Run';
+const EVENING_RUN_TITLE = IS_CHINESE ? '傍晚跑步' : 'Evening Run';
 const NIGHT_RUN_TITLE = IS_CHINESE ? '夜间跑步' : 'Night Run';
 
 const RUN_TITLES = {
