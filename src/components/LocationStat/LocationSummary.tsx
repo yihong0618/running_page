9bfba59bda3f9f8c251738e3e841b5f76fa3d632
JiuGoe <lxp_xcn@hotmail.com>
2023-12-15 14:02:09 +0800

    Update LocationSummary.tsx

--- src/components/LocationStat/LocationSummary.tsx
+++ src/components/LocationStat/LocationSummary.tsx
@@ -15,7 +15,7 @@ const LocationSummary = () => {
           <Stat value={Object.keys(cities).length} description=" 个城市" />
         ) : null}
       </section>
-      <hr color="red" />
+      <hr color="white" />
     </div>
   );
 };
