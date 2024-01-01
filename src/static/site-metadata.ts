ea5606ad9eaaa9150723b656e3c40921b46027ba
tonidong <tonidong@tencent.com>
2024-01-01 17:25:50 +0800

    feat: change page icon

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: "Echo's Running Page",
-  siteUrl: 'https://donghao526.github.io/running/',
-  logo: 'https://media.istockphoto.com/id/1158723576/vector/running-man-icon-sign-flat.jpg?s=612x612&w=0&k=20&c=Nfj6k5NvsAdx9nS5JeqrK_tkpVvJ1pDHZfe7mqSvMOU=',
+  siteTitle: 'Running Page',
+  siteUrl: 'https://yihong.run',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://donghao526.github.io/',
+      url: 'https://github.com/yihong0618/gitblog',
     },
     {
       name: 'About',
-      url: 'https://github.com/donghao526',
+      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
     },
   ],
 };
