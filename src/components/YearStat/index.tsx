b7445913442f745f97411ae9e067e3246bb487dc
orionna319 <orionna@163.com>
2024-01-16 00:03:16 +0800

    fix: svg not found causes page crash

--- src/components/YearStat/index.tsx
+++ src/components/YearStat/index.tsx
@@ -1,24 +1,18 @@
-import { ComponentType, lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import Stat from '@/components/Stat';
 import useActivities from '@/hooks/useActivities';
 import { formatPace } from '@/utils/utils';
 import styles from './style.module.scss';
 import useHover from '@/hooks/useHover';
-import { yearStats } from '@assets/index'
+import { yearStats } from '@assets/index';
+import { loadSvgComponent } from '@/utils/svgUtils';
 
 const YearStat = ({ year, onClick }: { year: string, onClick: (_year: string) => void }) => {
   let { activities: runs, years } = useActivities();
   // for hover
   const [hovered, eventHandlers] = useHover();
   // lazy Component
-  const YearSVG = lazy(() =>
-    yearStats[`./year_${year}.svg`]()
-      .then((res) => ({ default: res as ComponentType<any> }))
-      .catch((err) => {
-        console.error(err);
-        return { default: () => <div>Failed to load SVG</div> };
-      })
-  );
+  const YearSVG = lazy(() => loadSvgComponent(yearStats, `./year_${year}.svg`));
 
   if (years.includes(year)) {
     runs = runs.filter((run) => run.start_date_local.slice(0, 4) === year);
