391edf1dae27bde1735598f3fad7714b4f8f2954
Dingkun Yang <dingkun.yang@duke.edu>
2024-02-01 15:11:26 -0500

    change url

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,13 +11,13 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: "Dingkun's Running Page",
-  siteUrl: 'https://yer1k.github.io/running_page/',
+  siteUrl: 'https://running.yer1k.com',
   logo: 'https://yer1k.gitlab.io/website/images/avatar.png',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://yer1k.gitlab.io/website/',
+      url: 'https://yer1k.com/',
     },
     {
       name: 'About',
