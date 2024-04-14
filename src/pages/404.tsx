48026b7fabb563a5c1edff82fca6c25bfe38053c
tianheg <i@tianheg.co>
2024-04-14 19:52:35 +0800

    format files with prettier

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
   const { siteUrl } = useSiteMetadata();
   return (
     <Layout>
-      <h1 className="text-5xl my-2.5 font-bold italic">404</h1>
+      <h1 className="my-2.5 text-5xl font-bold italic">404</h1>
       <p>This page doesn&#39;t exist.</p>
       <p className="text-gray-400">
         If you wanna more message, you could visit{' '}
