a5e0793df712c6d6ab39d0e0c51991b16b6d9c46
chensoul <ichensoul@gmail.com>
2024-04-28 07:14:34 +0800

    ChenSoul Run

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,14 +10,14 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Chensoul Running',
-  siteUrl: 'https://run.chensou.com',
+  siteTitle: 'ChenSoul Run',
+  siteUrl: 'https://run.chensou.cc',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://blog.chensoul.com/about',
+      url: 'https://blog.chensoul.cc',
     }
   ],
 };
