07b123fa78983071df5cc1de24f580a4f6032192
JiuGoe <lxp_xcn@hotmail.com>
2024-05-20 22:17:11 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Records',
   siteUrl: 'https://running.danilelxp.com',
-  logo: 'https://www.danilelxp.com/wp-content/uploads/2023/12/images.png',
+  logo: 'https://www.jiugoe.com/wp-content/uploads/2023/12/images.png',
   description: 'Just running',
   navLinks: [
     {
