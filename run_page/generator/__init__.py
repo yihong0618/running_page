e6526476e411ea3035b02108455c13a4593a69c7
F4ria <f4riacool@gmail.com>
2024-08-06 15:16:05 +0800

    Modified `Track` class to accept an optional `run_from` parameter in `to_namedtuple` method, (#702)
    allowing dynamic naming of activities based on their source.

--- run_page/generator/__init__.py
+++ run_page/generator/__init__.py
@@ -84,7 +84,9 @@ class Generator:
         synced_files = []
 
         for t in tracks:
-            created = update_or_create_activity(self.session, t.to_namedtuple())
+            created = update_or_create_activity(
+                self.session, t.to_namedtuple(run_from=file_suffix)
+            )
             if created:
                 sys.stdout.write("+")
             else:
