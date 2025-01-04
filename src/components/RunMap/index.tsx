f86e5e4cf86ca6917f630b289ac0d0a379a83b49
Dingkun Yang <dingkun.yang@duke.edu>
2025-01-04 14:26:39 -0500

    update map style version

--- src/components/RunMap/index.tsx
+++ src/components/RunMap/index.tsx
@@ -145,7 +145,7 @@ const RunMap = ({
       {...viewState}
       onMove={onMove}
       style={style}
-      mapStyle="mapbox://styles/mapbox/dark-v10"
+      mapStyle="mapbox://styles/mapbox/dark-v11"
       ref={mapRefCallback}
       mapboxAccessToken={MAPBOX_TOKEN}
     >
