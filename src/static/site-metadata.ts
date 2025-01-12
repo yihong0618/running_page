aa28589465f06848ea965d738805fad69aca8390
gthjtjj <69075575+gthjtjj@users.noreply.github.com>
2025-01-12 15:43:09 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,13 +11,13 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://gthjtjj.github.io/snail_running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: '小红书',
+      url: 'https://www.xiaohongshu.com/user/profile/5b3b2a25f7e8b91a5561c4d8?xsec_token=YBSTvVlqAmEu-lZkFUYfEPD_uU0v6VaJExdJbT6KrdaxM=&xsec_source=app_share&xhsshare=CopyLink&appuid=5b3b2a25f7e8b91a5561c4d8&apptime=1736653918&share_id=245e0b668f664cfa9aa2831d59e0ebd7',
     },
     {
       name: 'About',
