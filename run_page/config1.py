d6b69eba0bdf4a2635304554a7122e33c7de5009
danielyu316 <134822806+danielyu316@users.noreply.github.com>
2025-01-15 17:07:08 +0800

    update config.py

--- run_page/config1.py
+++ run_page/config1.py
@@ -20,7 +20,6 @@ SQL_FILE = os.path.join(parent, "run_page", "data.db")
 JSON_FILE = os.path.join(parent, "src", "static", "activities.json")
 SYNCED_FILE = os.path.join(parent, "imported.json")
 SYNCED_ACTIVITY_FILE = os.path.join(parent, "synced_activity.json")
-NAME_MAPPING_FILE = os.path.join(FIT_FOLDER, "name_mapping.json")
 
 # TODO: Move into nike_sync NRC THINGS
 
@@ -51,53 +50,10 @@ def config(*keys):
 
 
 # add more type here
-TYPE_DICT = {
-    "running": "Run",
-    "RUN": "Run",
-    "Run": "Run",
-    "track_running": "Run",
-    "trail_running": "Trail Run",
-    "cycling": "Ride",
-    "CYCLING": "Ride",
-    "Ride": "Ride",
-    "EBikeRide": "Ride",
-    "road_biking": "Ride",
-    "VirtualRide": "VirtualRide",
-    "indoor_cycling": "Indoor Ride",
-    "walking": "Hike",
-    "hiking": "Hike",
-    "Walk": "Hike",
-    "Hike": "Hike",
-    "Swim": "Swim",
-    "rowing": "Rowing",
-    "RoadTrip": "RoadTrip",
-    "flight": "Flight",
-    "kayaking": "Kayaking",
-    "Snowboard": "Snowboard",
-    "resort_skiing_snowboarding_ws": "Ski",  # garmin
-    "AlpineSki": "Ski",  # strava
-    "Ski": "Ski",
-}
-
-MAPPING_TYPE = [
-    "Hike",
-    "Ride",
-    "VirtualRide",
-    "Rowing",
-    "Run",
-    "Trail Run",
-    "Swim",
-    "RoadTrip",
-    "Kayaking",
-    "Snowboard",
-    "Ski",
-]
-
 STRAVA_GARMIN_TYPE_DICT = {
     "Hike": "hiking",
     "Run": "running",
     "EBikeRide": "cycling",
-    "VirtualRide": "VirtualRide",
     "Walk": "walking",
     "Swim": "swimming",
 }
