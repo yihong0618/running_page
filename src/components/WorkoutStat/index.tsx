43dd59419926b89f0113b19baaf52d9be2c64502
danielyu316 <134822806+danielyu316@users.noreply.github.com>
2025-01-16 15:06:53 +0800

    Update index.tsx

--- src/components/WorkoutStat/index.tsx
+++ src/components/WorkoutStat/index.tsx
@@ -5,12 +5,12 @@ import { MAIN_COLOR } from '@/utils/const';
 const WorkoutStat = ({value, description, pace, className, distance, onClick, color=MAIN_COLOR}:
                          { value: string, description:string, pace: string, className: string, distance: string, onClick: (_year: string) => void , color: string}) =>
     (<div className={`${className || " "} pb-2 w-100`} onClick={onClick} style={{'color': color}}>
-    <span className={`text-3xl font-bold italic`}>{intComma(value)}</span>
+    <span className={`text-2xl font-bold italic`}>{intComma(value)}</span>
     <span className="text-l font-semibold italic">{description}</span>
-    { pace && (<span className="text-3xl font-bold italic">{ " " +pace}</span>)}
+    { pace && (<span className="text-2xl font-bold italic">{ " " +pace}</span>)}
     { pace && (<span className="text-l font-semibold italic"> Pace</span>)}
 
-    { distance && (<span className="text-3xl font-bold italic">{ " " + distance}</span>)}
+    { distance && (<span className="text-2xl font-bold italic">{ " " + distance}</span>)}
     { distance && (<span className="text-l font-semibold italic"> KM</span>)}
 
   </div>
