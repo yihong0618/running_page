d37cc75874717bed9eb68e1649768d5a31548849
Floyd Wang <gassnake999@gmail.com>
2025-03-10 11:29:49 +0800

    chore: Update links (#1)

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: `Let's fxxking run`,
+  siteUrl: 'https://www.madcodelife.com',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: `Floyd Wang's running page`,
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://www.madcodelife.com/blog',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://www.madcodelife.com',
     },
   ],
 };
