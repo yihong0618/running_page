8da6be93913429d016f6c458f3af5f8ab4a7877b
John Brandon <132646358+malanxi2022@users.noreply.github.com>
2025-04-05 17:19:41 +0800

    feat: Add summary page (#750)
    * feat: Add summary page
    
    * fix: some style
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    
    ---------
    
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>
    Co-authored-by: mx <480471449@qq.com>
    Co-authored-by: yihong0618 <zouzou0208@gmail.com>

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,6 +16,10 @@ const data: ISiteMetadataResult = {
   description: 'Personal site and blog',
   navLinks: [
     {
+      name: 'Summary',
+      url: '/summary',
+    },
+    {
       name: 'Blog',
       url: 'https://github.com/yihong0618/gitblog',
     },
