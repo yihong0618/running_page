af62db64af9f1b316cd21bf37f4e6e701733726b
yihong <zouzou0208@gmail.com>
2025-04-21 10:31:41 +0800

    fix: some typos (#815)
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- run_page/garmin_to_strava_sync.py
+++ run_page/garmin_to_strava_sync.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
         "--is-cn",
         dest="is_cn",
         action="store_true",
-        help="if garmin accout is cn",
+        help="if garmin account is cn",
     )
     parser.add_argument(
         "--tcx",
