4888bba917787654c1e4a0a03dfef70f926ef9af
yihong0618 <zouzou0208@gmail.com>
2025-04-30 17:08:48 +0800

    feat: auto share png
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- run_page/config.py
+++ run_page/config.py
@@ -10,6 +10,7 @@ OUTPUT_DIR = os.path.join(parent, "activities")
 GPX_FOLDER = os.path.join(parent, "GPX_OUT")
 TCX_FOLDER = os.path.join(parent, "TCX_OUT")
 FIT_FOLDER = os.path.join(parent, "FIT_OUT")
+PNG_FOLDER = os.path.join(parent, "PNG_OUT")
 ENDOMONDO_FILE_DIR = os.path.join(parent, "Workouts")
 FOLDER_DICT = {
     "gpx": GPX_FOLDER,
