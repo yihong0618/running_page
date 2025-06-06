e180b2fc4938141407c1d45ae1bf7de38d02e332
maslke <maslke@outlook.com>
2025-06-07 00:53:14 +0800

    change settings.

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -45,9 +45,9 @@ const PRIVACY_MODE = false;
 //set to `false` if you want to make light off as default, only effect when `PRIVACY_MODE` = false
 const LIGHTS_ON =true;
 //set to `true` if you want to show the 'Elevation Gain' column
-const SHOW_ELEVATION_GAIN = false;
+const SHOW_ELEVATION_GAIN = true;
 // richer title for the activity types (like garmin style)
-const RICH_TITLE = false;
+const RICH_TITLE = true;
 
 // IF you outside China please make sure IS_CHINESE = false
 const IS_CHINESE = true;
