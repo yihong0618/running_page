da420d29fb52c5896275f4024f3c605f2f048c22
maslke <maslke@outlook.com>
2025-06-09 22:40:26 +0800

    feat: Allow users to scroll the page over the map on touch devices

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -435,6 +435,8 @@ const getMapStyle = (vendor: string, styleName: string, token: string) => {
   return style;
 };
 
+const isTouchDevice = () => 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
 export {
   titleForShow,
   formatPace,
@@ -455,4 +457,5 @@ export {
   formatRunTime,
   convertMovingTime2Sec,
   getMapStyle,
+  isTouchDevice,
 };
