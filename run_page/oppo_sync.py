2bceb62b5fa808ff3794724a62053b9e7ce90321
yihong0618 <zouzou0208@gmail.com>
2025-06-10 09:03:19 +0800

    fix: remove dead code
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- run_page/oppo_sync.py
+++ run_page/oppo_sync.py
@@ -26,11 +26,6 @@ from generator import Generator
 from utils import adjust_time
 
 TOKEN_REFRESH_URL = "https://sport.health.heytapmobi.com/open/v1/oauth/token"
-OPPO_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-}
 
 # Query brief version of sports records
 # The query range cannot exceed one month!
@@ -75,8 +70,6 @@ BRIEF_SPORT_DATA_API = "https://sport.health.heytapmobi.com/open/v1/data/sport/r
 # Query detailed sports records
 # The query range cannot exceed one day!
 DETAILED_SPORT_DATA_API = "https://sport.health.heytapmobi.com/open/v2/data/sport/record?startTimeMillis={start_time}&endTimeMillis={end_time}"
-
-TIMESTAMP_THRESHOLD_IN_MILLISECOND = 5000
 
 # If your points need trans from gcj02 to wgs84 coordinate which use by Mapbox
 TRANS_GCJ02_TO_WGS84 = True
--- run_page/strava_to_garmin_sync.py
+++ run_page/strava_to_garmin_sync.py
@@ -1,74 +1,11 @@
 import argparse
 import asyncio
-from datetime import datetime, timedelta
-from xml.etree import ElementTree
+from datetime import datetime
 
-import gpxpy
-import gpxpy.gpx
 from garmin_sync import Garmin
 from strava_sync import run_strava_sync
 from stravaweblib import DataFormat, WebClient
 from utils import make_strava_client
-
-
-def generate_strava_run_points(start_time, strava_streams):
-    """
-    strava return same len data list
-    """
-    if not (strava_streams.get("time") and strava_streams.get("latlng")):
-        return None
-    points_dict_list = []
-    time_list = strava_streams["time"].data
-    time_list = [start_time + timedelta(seconds=int(i)) for i in time_list]
-    latlng_list = strava_streams["latlng"].data
-
-    for i, j in zip(time_list, latlng_list):
-        points_dict_list.append(
-            {
-                "latitude": j[0],
-                "longitude": j[1],
-                "time": i,
-            }
-        )
-    # add heart rate
-    if strava_streams.get("heartrate"):
-        heartrate_list = strava_streams.get("heartrate").data
-        for index, h in enumerate(heartrate_list):
-            points_dict_list[index]["heart_rate"] = h
-    # add altitude
-    if strava_streams.get("altitude"):
-        heartrate_list = strava_streams.get("altitude").data
-        for index, h in enumerate(heartrate_list):
-            points_dict_list[index]["elevation"] = h
-    return points_dict_list
-
-
-def make_gpx_from_points(title, points_dict_list):
-    gpx = gpxpy.gpx.GPX()
-    gpx.nsmap["gpxtpx"] = "http://www.garmin.com/xmlschemas/TrackPointExtension/v1"
-    gpx_track = gpxpy.gpx.GPXTrack()
-    gpx_track.name = title
-    gpx_track.type = "Run"
-    gpx.tracks.append(gpx_track)
-
-    # Create first segment in our GPX track:
-    gpx_segment = gpxpy.gpx.GPXTrackSegment()
-    gpx_track.segments.append(gpx_segment)
-    for p in points_dict_list:
-        if p.get("heart_rate") is None:
-            point = gpxpy.gpx.GPXTrackPoint(**p)
-        else:
-            heart_rate_num = p.pop("heart_rate")
-            point = gpxpy.gpx.GPXTrackPoint(**p)
-            gpx_extension_hr = ElementTree.fromstring(
-                f"""<gpxtpx:TrackPointExtension xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1">
-                <gpxtpx:hr>{heart_rate_num}</gpxtpx:hr>
-                </gpxtpx:TrackPointExtension>
-            """
-            )
-            point.extensions.append(gpx_extension_hr)
-        gpx_segment.points.append(point)
-    return gpx.to_xml()
 
 
 async def upload_to_activities(
