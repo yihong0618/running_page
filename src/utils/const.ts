ec67eb542ba8d3d43daac8ffceb866f645716d28
maslke <maslke@outlook.com>
2025-06-27 00:09:21 +0800

    feat: change map tile style.

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -170,10 +170,10 @@ export const SWIMMING_COLOR = 'rgb(255,51,51)';
 
 // map tiles vendor, maptiler or mapbox or stadiamaps
 // if you want to use maptiler, set the access token in MAP_TILE_ACCESS_TOKEN
-export const MAP_TILE_VENDOR = "maptiler";
+export const MAP_TILE_VENDOR = "stadiamaps";
 
 // map tiles style name, see MAP_TILE_STYLES for more details
-export const MAP_TILE_STYLE = "winter-dark";
+export const MAP_TILE_STYLE = "alidade_satellite";
 
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
@@ -198,7 +198,8 @@ export const MAP_TILE_STYLES = {
   mapbox: {
     "dark-v10": "mapbox://styles/mapbox/dark-v10",
     "dark-v11": "mapbox://styles/mapbox/dark-v11",
-    "navigation-night": "mapbox://styles/mapbox/navigation-night-v1"
+    "navigation-night": "mapbox://styles/mapbox/navigation-night-v1",
+    "satellite-streets-v12": "mapbox://styles/mapbox/satellite-streets-v12"
   },
   default: "mapbox://styles/mapbox/dark-v10"
 }
