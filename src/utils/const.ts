499de38ce3f4947ceff8cf97a4834b7e445e530e
maslke <maslke@outlook.com>
2025-06-27 10:32:51 +0800

    fix: set the valid token.

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -178,7 +178,7 @@ export const MAP_TILE_STYLE = "alidade_satellite";
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
 // stadiamaps: 8a769c5a-9125-4936-bdcf-a6b90cb5d0a4 |sign up at https://client.stadiamaps.com/signup/
-export const MAP_TILE_ACCESS_TOKEN = "Gt5R0jT8tuIYxW6sNrAg";
+export const MAP_TILE_ACCESS_TOKEN = "8a769c5a-9125-4936-bdcf-a6b90cb5d0a4";
 
 export const MAP_TILE_STYLES = {
   maptiler: {
