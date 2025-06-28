5374fdb5192f05d2b84a9600e686e578e5b8b418
Dingkun Yang <dingkun.yang@duke.edu>
2025-06-28 10:00:46 -0400

    fix: add empty check to prevent IndexError on empty run_points_data

--- run_page/garmin_sync.py
+++ run_page/garmin_sync.py
@@ -19,7 +19,7 @@ import aiofiles
 import garth
 import httpx
 from config import FOLDER_DICT, JSON_FILE, SQL_FILE
-from garmin_device_adaptor import wrap_device_info
+from garmin_device_adaptor import process_garmin_data
 from utils import make_activities_file
 
 # logging.basicConfig(level=logging.DEBUG)
@@ -132,16 +132,11 @@ class Garmin:
             use_fake_garmin_device,
         )
         for data in datas:
-            print(data.filename)
             with open(data.filename, "wb") as f:
                 for chunk in data.content:
                     f.write(chunk)
             f = open(data.filename, "rb")
-            # wrap fake garmin device to origin fit file, current not support gpx file
-            if use_fake_garmin_device:
-                file_body = wrap_device_info(f)
-            else:
-                file_body = BytesIO(f.read())
+            file_body = process_garmin_data(f, use_fake_garmin_device)
             files = {"file": (data.filename, file_body)}
 
             try:
