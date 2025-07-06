ddcce2608e29e6e37c30805cd9ea04823e1979f5
toddlerya <toddlerya@gmail.com>
2025-07-06 16:57:36 +0800

    增加同步脚本，gitignore规则

---
+++ sync_keep_to_strava.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ set -a
+source .env
+set +a
+python run_page/keep_to_strava_sync.py ${keep_mobile} ${keep_password} ${client_id} ${client_secret} ${strava_refresh_token} --sync-types running cycling hiking
