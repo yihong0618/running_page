f712257fa0d832635cb5900d1c22051518f2cbab
toddlerya <toddlerya@gmail.com>
2025-07-07 15:49:52 +0800

    gen_total_data_analysis.sh

--- gen_total_data_analysis.sh
+++ gen_total_data_analysis.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+ set -a
+source .env
+set +a
 python3 run_page/gen_svg.py --from-db --type circular --use-localtime
-python3 run_page/gen_svg.py --from-db --type monthoflife --birth 1992-06 --special-distance 5 --special-distance2 10 --special-color '#f9d367'  --special-color2 '#f0a1a8' --output assets/mol.svg --use-localtime --athlete gumuxi --title 'Runner Month of Life'
+python3 run_page/gen_svg.py --from-db --type monthoflife --birth ${birth} --special-distance ${special_distance} --special-distance2 ${special_distance2} --special-color '#f9d367'  --special-color2 '#f0a1a8' --output assets/mol.svg --use-localtime --athlete ${athlete} --title 'Runner Month of Life'
