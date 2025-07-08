01757b0c3e1e9709aeee4c1e0d2677fc2f8a60a1
Dingkun Yang <dingkun.yang@duke.edu>
2025-07-08 09:50:25 -0400

    classify sport type

--- run_page/keep_to_strava_sync.py
+++ run_page/keep_to_strava_sync.py
@@ -3,12 +3,13 @@ import json
 import os
 import time
 from collections import namedtuple
-from config import GPX_FOLDER
-from config import OUTPUT_DIR
-from stravalib.exc import ActivityUploadFailed, RateLimitTimeout
-from utils import make_strava_client, upload_file_to_strava
+
+from config import GPX_FOLDER, OUTPUT_DIR
 from keep_sync import KEEP_SPORT_TYPES, get_all_keep_tracks
 from strava_sync import run_strava_sync
+from stravalib.exc import ActivityUploadFailed, RateLimitTimeout
+
+from utils import make_strava_client, upload_file_to_strava
 
 """
 Only provide the ability to sync data from Keep's multiple sport types to Strava's corresponding sport types to help those who use multiple devices like me, the web page presentation still uses Strava (or refer to nike_to_strava_sync.py to modify it to suit you).
@@ -140,5 +141,8 @@ if __name__ == "__main__":
             continue
 
     run_strava_sync(
-        options.client_id, options.client_secret, options.strava_refresh_token
+        options.client_id,
+        options.client_secret,
+        options.strava_refresh_token,
+        sync_types=options.sync_types,
     )
