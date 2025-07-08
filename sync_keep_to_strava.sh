7ae0de68a600707b7aa2c7d81f3d231c209743bf
toddlerya <toddlerya@gmail.com>
2025-07-08 10:18:54 +0800

    同步所有类型

--- sync_keep_to_strava.sh
+++ sync_keep_to_strava.sh
@@ -2,4 +2,4 @@
  set -a
 source .env
 set +a
-python run_page/keep_to_strava_sync.py ${keep_mobile} ${keep_password} ${client_id} ${client_secret} ${strava_refresh_token} --sync-types running hiking
+python run_page/keep_to_strava_sync.py ${keep_mobile} ${keep_password} ${client_id} ${client_secret} ${strava_refresh_token} --sync-types  running cycling hiking
