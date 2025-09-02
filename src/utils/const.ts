28abb6d0ec00b65196fba7fc3040b0e81a3bf49b
maslke <maslke@outlook.com>
2025-09-02 10:42:37 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ const MAPBOX_TOKEN =
   // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'pk.eyJ1IjoibWFzbGtlIiwiYSI6ImNreGhiZ2M1bzA5MDQycHF3Nms2NjNnMzAifQ.lSJ41OQc9dgA6okX_PzbTQ';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
