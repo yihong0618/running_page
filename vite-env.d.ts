b403479e11dec6c7738dcd112a2e2a4fb7f23f40
shaonianche <github@duanfei.org>
2025-09-22 11:30:24 +0800

    refactor: make mapbox token to secret
    Signed-off-by: shaonianche <github@duanfei.org>

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_MAPBOX_TOKEN?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
