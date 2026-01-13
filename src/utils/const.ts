1dee4e840e67a112b6a490a85f928bff43f9d4fc
StoneRicky <739799980@qq.com>
2026-01-13 21:45:42 +0800

    更改信息

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ const MAPBOX_TOKEN =
   // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJjbWYxdXR4YncwMTJtMm5zOTE4eTZpMGdtIn0.OnsXdwkZFztR8a5Ph_T-xg';
+  'pk.eyJ1Ijoic3RvbmVyaWNreSIsImEiOiJjbWtjZjNsZ2owMDVpM2ZwdzZ1b2E5ajd6In0.x7nGC4a2wkN7WoX1fs3MlA';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
@@ -285,5 +285,5 @@ export const MAP_TILE_STYLES = {
     'navigation-night': 'mapbox://styles/mapbox/navigation-night-v1',
     'satellite-streets-v12': 'mapbox://styles/mapbox/satellite-streets-v12',
   },
-  default: 'mapbox://styles/mapbox/dark-v10',
+  default: 'mapbox://styles/mapbox/light-v11',
 };
