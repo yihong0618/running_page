744d1e80e3c6ec8838d3c8c7f4d672fa55887661
StoneRicky <739799980@qq.com>
2026-01-14 20:12:09 +0800

    修改地图

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -230,7 +230,10 @@ export const SWIMMING_COLOR = 'rgb(255,51,51)';
 
 // map tiles vendor, maptiler or mapbox or stadiamaps
 // if you want to use maptiler, set the access token in MAP_TILE_ACCESS_TOKEN
-export const MAP_TILE_VENDOR = 'mapbox';
+// "mapbox" - Mapbox map services
+// "maptiler" - MapTiler map services
+// "stadiamaps" - Stadia Maps service
+export const MAP_TILE_VENDOR = 'maptiler';
 
 // map tiles style name, see MAP_TILE_STYLES for more details
 export const MAP_TILE_STYLE_LIGHT = 'light-v10';
@@ -239,7 +242,8 @@ export const MAP_TILE_STYLE_DARK = 'dark-v10';
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
 // stadiamaps: 8a769c5a-9125-4936-bdcf-a6b90cb5d0a4 | sign up at https://client.stadiamaps.com/signup/
-export const MAP_TILE_ACCESS_TOKEN = 'Gt5R0jT8tuIYxW6sNrAg';
+// export const MAP_TILE_ACCESS_TOKEN = 'Gt5R0jT8tuIYxW6sNrAg';
+export const MAP_TILE_ACCESS_TOKEN = 'zsyILl2XTNXD1caH8NQf';
 
 export const MAP_TILE_STYLES = {
   maptiler: {
