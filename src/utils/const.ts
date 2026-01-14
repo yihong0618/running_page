656605eecf957395310954f9b5a88cbdce0adfd5
StoneRicky <739799980@qq.com>
2026-01-14 20:32:31 +0800

    更换地图

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -236,8 +236,8 @@ export const SWIMMING_COLOR = 'rgb(255,51,51)';
 export const MAP_TILE_VENDOR = 'maptiler';
 
 // map tiles style name, see MAP_TILE_STYLES for more details
-export const MAP_TILE_STYLE_LIGHT = 'light-v10';
-export const MAP_TILE_STYLE_DARK = 'dark-v10';
+export const MAP_TILE_STYLE_LIGHT = 'winter-light';
+export const MAP_TILE_STYLE_DARK = 'dataviz-light';
 
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
