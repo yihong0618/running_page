eed8a4373504dc5c02f92b3db25e5695adcc50e5
StoneRicky <739799980@qq.com>
2026-01-14 20:47:59 +0800

    格式更改，适合action

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -248,37 +248,29 @@ export const MAP_TILE_ACCESS_TOKEN = 'zsyILl2XTNXD1caH8NQf';
 export const MAP_TILE_STYLES = {
   maptiler: {
     'dataviz-light': 'https://api.maptiler.com/maps/dataviz/style.json?key=',
-    'dataviz-dark':
-      'https://api.maptiler.com/maps/dataviz-dark/style.json?key=',
+    'dataviz-dark': 'https://api.maptiler.com/maps/dataviz-dark/style.json?key=',
     'basic-light': 'https://api.maptiler.com/maps/basic-v2/style.json?key=',
     'basic-dark': 'https://api.maptiler.com/maps/basic-v2-dark/style.json?key=',
     'streets-light': 'https://api.maptiler.com/maps/streets-v2/style.json?key=',
-    'streets-dark':
-      'https://api.maptiler.com/maps/streets-v2-dark/style.json?key=',
+    'streets-dark': 'https://api.maptiler.com/maps/streets-v2-dark/style.json?key=',
     'outdoor-light': 'https://api.maptiler.com/maps/outdoor-v2/style.json?key=',
     'outdoor-light-v4': 'https://api.maptiler.com/maps/outdoor-v4/style.json?key=',
-    'outdoor-dark':
-      'https://api.maptiler.com/maps/outdoor-v2-dark/style.json?key=',
+    'outdoor-dark': 'https://api.maptiler.com/maps/outdoor-v2-dark/style.json?key=',
     'bright-light': 'https://api.maptiler.com/maps/bright-v2/style.json?key=',
-    'bright-dark':
-      'https://api.maptiler.com/maps/bright-v2-dark/style.json?key=',
+    'bright-dark': 'https://api.maptiler.com/maps/bright-v2-dark/style.json?key=',
     'topo-light': 'https://api.maptiler.com/maps/topo-v2/style.json?key=',
     'topo-dark': 'https://api.maptiler.com/maps/topo-v2-dark/style.json?key=',
     'winter-light': 'https://api.maptiler.com/maps/winter-v2/style.json?key=',
-    'winter-dark':
-      'https://api.maptiler.com/maps/winter-v2-dark/style.json?key=',
+    'winter-dark': 'https://api.maptiler.com/maps/winter-v2-dark/style.json?key=',
     hybrid: 'https://api.maptiler.com/maps/hybrid/style.json?key=',
   },
 
   // https://docs.stadiamaps.com/themes/
   stadiamaps: {
     // light
-    alidade_smooth:
-      'https://tiles.stadiamaps.com/styles/alidade_smooth.json?api_key=',
-    alidade_smooth_dark:
-      'https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json?api_key=',
-    alidade_satellite:
-      'https://tiles.stadiamaps.com/styles/alidade_satellite.json?api_key=',
+    alidade_smooth: 'https://tiles.stadiamaps.com/styles/alidade_smooth.json?api_key=',
+    alidade_smooth_dark: 'https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json?api_key=',
+    alidade_satellite: 'https://tiles.stadiamaps.com/styles/alidade_satellite.json?api_key=',
   },
 
   // https://docs.mapbox.com/api/maps/styles/
